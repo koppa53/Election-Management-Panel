42e73b3d4bb3a6d70129dfc36807e2ff25616833
koppa53 <jeffersonclemente.70@gmail.com>
2021-08-16 00:23:50 +0800

    Added Viewing of Votes Based on Election Year

--- assets/js/vote-records-table.js
+++ assets/js/vote-records-table.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
         data.forEach(function (v) {
             var o = new Option(v.candidate_election_year, v.candidate_election_year);
             var x = new Option(v.candidate_election_year, v.candidate_election_year);
+            var y = new Option(v.candidate_election_year, v.candidate_election_year);
+            $("#electionyear").append(y);
             $("#electionyear1").append(o);
             $("#electionyear2").append(x);
         })
@@ -30,12 +32,13 @@ $(document).ready(function () {
 
     $('#table1').DataTable({
         ajax: {
-            url: `http://localhost:5000/all_student_votes`,
+            url: `http://localhost:5000/all_student_voters_logs`,
             dataSrc: "",
             beforeSend: function (request) {
                 request.setRequestHeader("authorization", tok);
             }
         },
+        destroy: true,
         autoWidth: false,
         responsive: true,
         columns: [
@@ -57,12 +60,13 @@ $(document).ready(function () {
 
     $('#table2').DataTable({
         ajax: {
-            url: `http://localhost:5000/all_assisted_student_votes`,
+            url: `http://localhost:5000/all_assisted_student_voters_logs`,
             dataSrc: "",
             beforeSend: function (request) {
                 request.setRequestHeader("authorization", tok);
             }
         },
+        destroy: true,
         autoWidth: false,
         responsive: true,
         columns: [
@@ -90,4 +94,118 @@ $(document).ready(function () {
         order: [1, "asc"]
     });
 
+    $('#verifypassword').on('submit', async (event) => {
+        event.preventDefault();
+        const username = localStorage.getItem("User Name")
+        const password = $('#vpass').val();
+
+        try {
+            const response = await fetch('http://localhost:5000/verify_user_pass', {
+                method: "POST",
+                headers: {
+                    "authorization": tok,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            });
+            const d = await response.json();
+            if (response.ok) {
+                $('#showVotes').modal({
+                    backdrop: 'static',
+                    keyboard: false
+                });
+                $('#vpass').val("");
+                $('#authpass').modal('hide');
+                Toastify({
+                    text: d.message,
+                    duration: 3000,
+                    gravity: "top",
+                    position: "center",
+                    backgroundColor: "#4fbe87",
+                }).showToast();
+                $('#showVotes').modal('show');
+            } else {
+                Toastify({
+                    text: d.message,
+                    duration: 3000,
+                    gravity: "top",
+                    position: "center",
+                    backgroundColor: "#CD201F",
+                }).showToast();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    $('#electionyear').change(function () {
+        const electionyear = $('#electionyear').val()
+        $('#tableVotes').DataTable({
+            ajax: {
+                url: `http://localhost:5000/election_year_student_votes/` + electionyear,
+                dataSrc: "",
+                beforeSend: function (request) {
+                    request.setRequestHeader("authorization", tok);
+                }
+            },
+            destroy: true,
+            autoWidth: false,
+            responsive: true,
+            columns: [
+                { data: "vote_student_id" },
+                { data: "vote_candidate_first_name", visible: false },
+                { data: "vote_candidate_last_name", visible: false },
+                {
+                    mData: function (data) {
+                        return data.vote_candidate_last_name + " " + data.vote_candidate_first_name[0].toUpperCase() + ".";
+                    }
+                },
+                { data: "vote_candidate_position" },
+                { data: "vote_council" },
+                { data: "vote_college" },
+                {
+                    data: null,
+                    defaultContent: `<input type="checkbox" class="form-check-input" id="checkbox2">
+                                    <label for="checkbox2">Mark</label>`
+                }
+
+            ],
+            order: [0, "asc"]
+        });
+        $('#tableVotes2').DataTable({
+            ajax: {
+                url: `http://localhost:5000/election_year_assisted_student_votes/` + electionyear,
+                dataSrc: "",
+                beforeSend: function (request) {
+                    request.setRequestHeader("authorization", tok);
+                }
+            },
+            destroy: true,
+            autoWidth: false,
+            responsive: true,
+            columns: [
+                { data: "vote_student_id" },
+                { data: "vote_candidate_first_name", visible: false },
+                { data: "vote_candidate_last_name", visible: false },
+                {
+                    mData: function (data) {
+                        return data.vote_candidate_last_name + " " + data.vote_candidate_first_name[0].toUpperCase() + ".";
+                    }
+                },
+                { data: "vote_candidate_position" },
+                { data: "vote_council" },
+                { data: "vote_college" },
+                {
+                    data: null,
+                    defaultContent: `<input type="checkbox" class="form-check-input" id="checkbox2">
+                                    <label for="checkbox2">Mark</label>`
+                }
+
+            ],
+            order: [0, "asc"]
+        });
+    })
 });
